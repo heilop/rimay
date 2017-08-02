bf18f0d5f2048269bb6ea022f23aba64ae27e9d6
Heissen Lopez <heissen.lopez@gmail.com>
2017-08-01 20:39:43 -0500

    Separate Word list in another file

---
+++ words.js
@@ -0,0 +1,44 @@
+var wordList = [
+  {
+    id: 0,
+    word: "Tunche",
+    location: "Amazonas, Peru",
+    distance: 10.4,
+    usefulRating: 4,
+    comments: "This hike was nice and hike-like. Glad I didn't bring a bike."
+  },
+  {
+    id: 1,
+    word: "Ralo",
+    location: "Amazonas, Peru",
+    distance: 20.86,
+    usefulRating: 3,
+    comments: "Not the best, but would probably do again. Note to self: don't forget the sandwiches next time."
+  },
+  {
+    id: 2,
+    word: "Michuque",
+    location: "Amazonas, Peru",
+    distance: 8.2,
+    usefulRating: 5,
+    comments: "Short but SO sweet!!"
+  },
+  {
+    id: 3,
+    word: "Ishpa",
+    location: "Amazonas, Peru",
+    distance: 12.3,
+    usefulRating: 4,
+    comments: "Took my time with this one. Great view!"
+  },
+  {
+    id: 4,
+    word: "Choloque",
+    location: "Amazonas, Peru",
+    distance: 19.34,
+    usefulRating: 2,
+    comments: "Too long, too hot. Also that snakebite wasn't very fun."
+  }
+];
+
+module.exports = wordList;
