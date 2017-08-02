1fc5b17588b6f4a94adaafe99dc978f17a9a6520
Heissen Lopez <heissen.lopez@gmail.com>
2017-08-01 23:45:44 -0500

    Split views of our app separate components

--- Pages/EditWordPage.js
+++ Pages/EditWordPage.js
@@ -1,6 +1,6 @@
 // Import FuseJS' Observable module.
 var Observable = require("FuseJS/Observable");
-var wordList = require("words");
+
 // Variables will be use.
 var title = Observable('Register a new Word');
 var words = Observable();
@@ -9,20 +9,12 @@ var usefulRating = words.map(function(x) { return x.usefulRating; });
 var comments = words.map(function(x) { return x.comments; });
 var word = words.map(function(x) { return x.word; });
 
-function chooseWord(arg) {
-  words.value = arg.data;
-}
-
-
 // Expose the variables to Vview.
 module.exports = {
   title: title,
-  wordList: wordList,
 
   word: word,
   location: location,
   usefulRating: usefulRating,
-  comments: comments,
-
-  chooseWord: chooseWord
+  comments: comments
 }
---
+++ Pages/HomePage.js
@@ -0,0 +1,18 @@
+var wordList = require("words");
+
+function chooseWord(arg) {
+  //words.value = arg.data;
+}
+
+// Expose the variables to Vview.
+module.exports = {
+  // title: title,
+  wordList: wordList,
+  //
+  // word: word,
+  // location: location,
+  // usefulRating: usefulRating,
+  // comments: comments,
+
+  chooseWord: chooseWord
+}
